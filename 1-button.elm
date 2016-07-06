003c0369072725b7f48c99e46586c753199f6765
Jonathan Yankovich <jonathan.yankovich@gmail.com>
2016-07-05 23:44:34 -0400

    Add test.

--- 1-button.elm
+++ 1-button.elm
@@ -1,22 +1,24 @@
 import Html exposing (Html, button, div, text)
-import Html.App as Html
+-- import Html.App as Html
 import Html.Events exposing (onClick)
+import ElmTest exposing (..)
 
-main =
-  Html.beginnerProgram
-  { model = model
-  , view = view
-  , update = update
-  }
+--main =
+--  Html.beginnerProgram
+--  { model = model
+--  , view = view
+--  , update = update
+--  }
 
 
 type alias Model = {
   num: Int
+  , err: String
 }
 
 
 model : Model
-model = {num = 0}
+model = {num = 0, err = ""}
 
 
 type Msg = Increment | Decrement
@@ -26,14 +28,26 @@ update msg model =
   case msg of
     Increment ->
       if model.num >= 10 then
-        { num = 10 }
+        { model
+        | num = 10
+        , err = "TOO HIGH"
+        }
       else
-        { num = model.num + 1 }
+        { model
+        | num = model.num + 1
+        , err = ""
+      }
     Decrement ->
       if model.num > 0 then
-        { num = model.num - 1 }
+        { model
+        | num = model.num - 1
+        , err = ""
+        }
       else
-        { num = 0 }
+        { model
+        | num = 0
+        , err = "TOO LOW"
+        }
 
 
 view : Model -> Html Msg
@@ -41,6 +55,17 @@ view model =
   div []
    [ button [ onClick Decrement] [ text "-" ]
    , div [] [ text (toString model.num) ]
+   , div [] [ text (model.err) ]
    , button [ onClick Increment] [ text "+" ]
    ]
 
+-- Tests
+
+tests : Test
+tests =
+  suite "Our tests"
+    [ test "it can inc one" (assertEqual 1 (update Increment model).num ) ]
+
+main : Program Never
+main =
+    runSuite tests
